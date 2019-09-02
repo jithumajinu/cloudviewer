964bf545d1e4fab826b10fad0389a2bb0bee07ad
jithumajinu <jithu@ohboy.in>
2019-09-02 19:34:08 +0530

    server

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 8080;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+//res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
